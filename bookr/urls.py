6b3775da6a2da16218fd19875d25f4515e4bc297
udazzZZ <pirogova.dv96@mail.ru>
2023-10-18 02:16:44 +0300

    спринт 2

--- bookr/urls.py
+++ bookr/urls.py
@@ -23,5 +23,6 @@ import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', reviews.views.index)
+    path('', reviews.views.index),
+    path('book-search', reviews.views.book_search)
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'base.html')
 
+def book_search(request):
+    search_text = request.GET.get('search', '')
+    return render(request, 'bookr search template.html', {'search_text': search_text})
+
+
 
