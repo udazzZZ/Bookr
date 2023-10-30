252da77d471c36c0ed4792c38bdd7f19e5687be6
udazzZZ <pirogova.dv96@mail.ru>
2023-10-30 05:01:22 +0300

    3 sprint, 3 chapter

--- bookr/urls.py
+++ bookr/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 
 import reviews.views
@@ -24,5 +24,6 @@ import reviews.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', reviews.views.index),
-    path('book-search', reviews.views.book_search)
+    path('book-search/', reviews.views.book_search),
+    path('', include('reviews.urls')),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Book
+from .utils import average_rating
 
 
 def index(request):
-    return render(request, 'base.html')
+    return render(request, "reviews/base.html")
+
 
 def book_search(request):
-    search_text = request.GET.get('search', '')
-    return render(request, 'bookr search template.html', {'search_text': search_text})
+    search_text = request.GET.get("search", "")
+    return render(request, "bookr_search_template.html", {"search_text": search_text})
 
 
+def book_list(request):
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        book_list.append({'book': book,
+                          'book_rating': book_rating,
+                          'number_of_reviews': number_of_reviews})
 
+    context = {
+        'book_list': book_list
+    }
+    return render(request, 'reviews/books_list.html', context)
